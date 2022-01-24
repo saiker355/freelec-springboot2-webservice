f53bf14c89450971b0a6169db74543a2e05bf58a
saiker355 <saiker355@gmail.com>
2022-01-25 01:11:26 +0900

    test추가

---
+++ src/main/java/com/example/testing/web/dto/HelloResponseDto.java
@@ -0,0 +1,9 @@
+package com.example.testing.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+}
---
+++ src/test/java/com/example/testing/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,5 @@
+package com.example.testing.web.dto;
+
+public class HelloResponseDtoTest {
+
+}
